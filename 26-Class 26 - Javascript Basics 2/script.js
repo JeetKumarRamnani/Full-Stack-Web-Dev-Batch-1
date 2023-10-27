a416409f5617b959d7bcc868fb35b09a25c5ac22
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-10-27 15:48:32 +0500

    class 26 code and content completed

---
+++ 26-Class 26 - Javascript Basics 2/script.js
@@ -0,0 +1,42 @@
+// var jeet; // this is called declartion
+
+// jeet = 20; //this is called defination/initialization/assign  
+
+// var ajmal = 50;
+
+// ajmal =  ajmal+1;
+// document.write(ajmal)
+ 
+
+// var jeet = 100;  //variable declartion and defination
+
+
+//this is block
+{ //opening curly braces
+
+
+}  //closing curly braces
+
+
+
+
+
+
+
+
+
+
+
+// scope ==> global scope or local scope
+
+var jeet = 200; //global scope
+
+{
+
+    var jeet = 100; //local scope
+  document.write(jeet)
+    
+} //this is block
+
+
+//local scope==> block scope ,function scope 
