be0741389fc1b1ffddaa206b258081fa015ac9a9
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-11-02 01:00:36 +0500

    class 28 content and code added

---
+++ 28-Class 28 - Javascript Basics 4/script.js
@@ -0,0 +1,196 @@
+/* Javascript me 2 tareeke ke comments hote hain 
+ 1 hote hain single line 
+ or 1 hote hain multiline comments jinko block comments
+*/
+
+
+// console.log("Hello World")
+
+
+// ye saare kehlate hain opeartors
+
+// + 
+// -
+// /
+// *
+
+// 20 + 30 = 50;
+
+
+// Unary   Binary  And TERNARY 
+
+// ++
+// --
+// +
+// -
+
+
+
+
+var myNumber =  5;
+console.log(myNumber)
+myNumber++;  //6
+console.log(myNumber)
+myNumber++; //7
+console.log(myNumber)
+myNumber++; //8
+
+console.log(myNumber)
+
+
+
+
+
+//Unary increament Operator ++
+
+// var myNumber =  5;
+// console.log(myNumber)
+// myNumber++;  //6
+// console.log(myNumber)
+// myNumber++; //7
+// console.log(myNumber)
+// myNumber++; //8
+
+// console.log(myNumber)
+
+
+//Unary Decreament Operator --
+
+// var myNumber2 = 10;
+// myNumber2--;
+// console.log(myNumber2)
+
+//Binary Operation Operators 
+
+// Iske Andar Hote Hain  2 OPERAND
+
+// Binary Operator Ke Andar Ek Operators Ki Subcategory Aati hai jisko ham arthimatic opeartor kahte hain
+
+// Arthimatic Operators
+
+// +  = Addition
+// -  = Subtraction
+// /  = Division
+// *  = Multiplication
+// %  = Modulous
+
+
+// var operand1 = 50;
+// var operand2 = 20;
+
+// console.log(operand1 + operand2)
+// console.log(operand1 - operand2)
+// console.log(operand1 / operand2)
+// console.log(operand1 * operand2)
+// console.log(20 % 20)
+
+
+// // Binary Assignment Operators 
+
+// var myValue = 20;
+
+// // myValue = myValue * 10;
+
+// myValue*=10;
+
+// console.log(myValue)
+
+
+console.log(20+50/50*(40))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
