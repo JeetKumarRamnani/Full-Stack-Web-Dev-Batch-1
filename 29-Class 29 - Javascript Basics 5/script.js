24406b1448350e26ca0466aebda782e8d9bcdfcd
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-11-05 00:22:00 +0500

    class 29 complete code added

---
+++ 29-Class 29 - Javascript Basics 5/script.js
@@ -0,0 +1,19 @@
+// var fruit1 = "apple";
+// var fruit2 = "Mango";
+// var fruit3 = "grapes";
+// var fruit4 = "pineapple";
+
+// console.log(fruit4)
+
+
+var fruits = ["apple","Mango","grapes","Pineapple"];
+
+fruits[10] = "strawberry";
+                
+
+
+console.log(fruits[10])
+
+
+
+
