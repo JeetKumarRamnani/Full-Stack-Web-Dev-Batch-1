f7eec9c50bec197b75389874450c1bb0514bae4e
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-11-15 23:50:55 +0500

    class 31 all code and content uploaded

---
+++ 31-Class 31 - Javascript Basics 7/script.js
@@ -0,0 +1,38 @@
+// console.log(-6);
+
+// var ticketPrice = ---1;
+
+// if (ticketPrice == 500) {
+//   console.log('Here is your silver ticket');
+// } else if (ticketPrice == 1000) {
+//   console.log('Here is your golden ticket');
+// } else if (ticketPrice == 1500) {
+//   console.log('Here is your platinium ticket');
+// } else {
+//   console.log('Enter Correct Value qk hamare pass chutte nai hain');
+// }
+
+// console.log('I Am Out Of If Block');
+// var arr= [1,2,0,4,0] == [1,2,4,0,0]
+
+var Day = 'monday';
+
+switch (Day) {
+  case 'Monday':
+    console.log('Today is First Working Day');
+    break;
+
+  case 'Friday':
+    console.log('Yupee Weekend Ane Wala hai');
+    break;
+
+  case 'Tuesday':
+    console.log('tuesday');
+    break;
+  case 'Thursday':
+    console.log('thursday');
+    break;
+
+  default:
+    console.log('Main Hoon Default DON');
+}
