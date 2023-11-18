548d058fc6c73f150c774695ea4ff4ebc75eb7d2
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-11-18 23:47:36 +0500

    class 32 content and code all uploaded

---
+++ 32-Class 32 - Javascript Basics 8/script.js
@@ -0,0 +1,134 @@
+// var userInput = 6; //hardcoded value
+
+// // console.log(10%3);  //Modulous Operator iska kam hai hamein reminder value dena
+
+// // console.log(0)
+// // console.log(0 == "0");
+
+// if (0=== 0) {
+//   console.log('This is a even number');
+// } else {
+//   console.log('This is a odd number');
+// }
+
+// Assignment 3: Switch Statement
+// Problem: Write a program that converts a numeric day of the week (1 for Sunday, 2 for Monday, ..., 7 for Saturday) into its corresponding name.
+
+// Instructions:
+
+// Create a variable and assign a day number to it.
+// Use a switch statement to match the number with the corresponding day.
+// Print the day of the week.
+
+// var userInput = 8;
+
+// switch(userInput){
+//   case 1:
+//     console.log("Sunday")
+//     break;
+
+//     case 2:
+//     console.log("Monday")
+//     break;
+
+//     case 3:
+//     console.log("Tuesday")
+//     break;
+
+//     case 4:
+//     console.log("Wednesday")
+//     break;
+
+//     case 5:
+//     console.log("Thursday")
+//     break;
+
+//     case 6:
+//     console.log("Friday")
+//     break;
+//     case 7:
+//     console.log("Saturday")
+//     break;
+
+//     default:
+//       console.log("Enter Valid No From 1-7");
+// }
+
+//Nested If Else
+
+// var userAge = 17;
+// var nationality = 'Pakistan';
+
+// console.log('Welcome To Pakistan Voting System Here You Can Vote');
+
+// if (userAge >= 18) {
+//   if (nationality === 'Pakistan') {
+//     console.log('You can vote');
+//   } else {
+//     console.log(
+//       'You are older then 18 but you are not pakistani so you can not vote'
+//     );
+//   }
+// } else {
+//   console.log('You Cannot Vote because you fail age condition');
+// }
+
+// var userName= "";
+
+// userName = "Ameet Kumar"
+
+// if (-10) {
+//   console.log('YOU Are A USER');
+// } else {
+//   console.log('you are not a user');
+// }
+
+//Logical Operators
+
+// || OR Operator
+// && AND Operator
+
+// true || true
+// Logical OR Operator
+// 2>2 || 2===2
+//   F  ||      T   == true
+//  T  ||     T  == true
+// T    ||   T = true
+// F    ||   F = false
+
+// Logical AND Operator
+
+// T  && F = false
+// F && T = false
+// F && F = false
+// T && T = true
+
+// ! Unary Not Operaotr
+
+// if (2 === 2 && 11 > 10) {
+//   console.log('True Hai');
+// } else {
+//   console.log('False HAI BHAI');
+// }
+
+//Unary Not Operator
+
+// console.log(!true);
+
+// var isUserLoggedIn = true;
+
+// if (!isUserLoggedIn) {
+//   console.log('Welcome User Here iS Your Profile');
+// } else {
+//   console.log('please login ');
+// }
+
+// var result = 2 === 2 ? 'True' : 'False';
+// var result = '';
+
+// if (2 === 2) {
+//   result = 'True';
+// } else {
+//   result = 'False';
+// }
+// console.log(result);
