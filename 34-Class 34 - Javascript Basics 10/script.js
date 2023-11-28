1954f5d0c4efc22f24bfc05c1dc3a050574fb61b
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-11-28 17:46:35 +0500

    class 34 content code and assignment uploaded

---
+++ 34-Class 34 - Javascript Basics 10/script.js
@@ -0,0 +1,66 @@
+// var i = 1;
+
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// for (var i = 1; i <= 10; i++) {
+//   console.log(i);
+// // }
+// console.log('hello');
+// var userInput = 2;
+
+// for (var i = 0; i <= 10; i++) {
+//   console.log(userInput + 'X' + i + '=' + userInput * i);
+// }
+
+// var arr = [100, 50, 70, 80,90,60];
+
+// console.log(arr.length)
+
+// for(var i=0; i<arr.length; i++){
+//   console.log(arr[i])
+// }
+
+//Nesting Of Loops
+
+// for (var i = 0; i < 5; i++) {
+//   //Outer Loop
+//   console.log('Outer Loop');
+//   for (var j = 0; j < 5; j++) {
+//     //inner Loop
+//     console.log('inner Loop');
+//   }
+// }
+
+// for(var i=0; i<10; i++){
+//   console.log(i)
+// }
+
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+// console.log("Hello World")
+
+// function printHelloWorldFiveTimes() {
+//     console.log('Hello World');
+//     console.log('Hello World');
+//     console.log('Hello World');
+//     console.log('Hello World');
+//     console.log('Hello World');
+//   }
+  
