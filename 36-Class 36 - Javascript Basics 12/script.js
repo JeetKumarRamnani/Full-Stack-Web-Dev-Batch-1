58423ed2267d263e036fc12f0c1b220f7bf7aff0
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-12-03 01:08:02 +0500

    class 36 code uploaded

---
+++ 36-Class 36 - Javascript Basics 12/script.js
@@ -0,0 +1,204 @@
+// // Online Javascript Editor for free
+// // Write, Edit and Run your Javascript code using JS Online Compiler
+// { is block ke andar aap koi bhi javascript ki statement likh sakte ho }
+
+// {
+// var value1 = 20;
+// var value2 = 40;
+// var result = value1 + value2;
+// console.log(result);
+// }
+
+// {
+// var value3 = 100;
+// var value4 = 20;
+// var result2 = value3 + value4;
+// console.log(result2);
+// }
+
+// {
+//     //pateela lo
+//     //dhoodh dalo
+//     //pani dalo
+//     //choole ko aag lagao
+//     //pateela choole pe rakho
+//     //or jab chaye ban jae toh serve kardo
+//     //ye lo aapki chaye
+// }
+
+// var value5 = 300;
+// var value6 = 60;
+// var result3 = value5 + value6;
+// console.log(result3);
+
+// var value5 = 300;
+// var value6 = 60;
+// var result3 = value5 + value6;
+// console.log(result3);
+
+// var value5 = 300;
+// var value6 = 60;
+// var result3 = value5 + value6;
+// console.log(result3);
+
+// var value5 = 300;
+// var value6 = 60;
+// var result3 = value5 + value6;
+// console.log(result3);
+
+// var value5 = 300;
+// var value6 = 60;
+// var result3 = value5 + value6;
+// console.log(result3);
+
+// var parameter1; //declare kia hai
+
+// parameter1 = 20; //define karrha hoon
+
+// console.log(parameter1)
+
+// //Parameteres
+// function addition(parametere1) {
+//   console.log(parametere1);
+// }
+// //Arguments
+// addition(12);
+// var value1;
+// var value2;
+
+// function multiplication(xyz, hh, sas, sa, qw) {
+//   console.log(xyz,hh,sas,sa,qw);
+
+// }
+
+// multiplication(50, 100, 70, 2, 30);
+
+// function greetMember(userName) {
+//   console.log('Welcome ' + userName);
+// }
+
+// greetMember('Jeet');
+// greetMember('Saad');
+// greetMember('Khizra');
+// function addition(parameter1, parameter2) {
+//   var value1 = parameter1;
+//   var value2 = parameter2;
+//   var result = value1 + value2;
+//   console.log(result);
+// }
+
+// addition(60, 40); //100
+// addition(100, 20); //120
+// addition(100, 40); //140
+// addition(60, 100); //160
+// addition(10, 100); //160
+// addition(1000, 100); //160
+// addition(20000, 100); //160
+
+// Ab Mera Function Hochuka Hai Dynamic
+
+// function multiply(value1,value2) {
+//   console.log(value1*value2)
+//   return value1 * value2
+// }
+
+// multiply(5,4); // 20
+// multiply(10,6); // 60
+
+//Declaration
+// var v1;
+// var v2;
+
+// //Defination
+// v1= 10;
+// v2=40;
+
+// var v3 = 10;
+
+// Par functions ke lie defination and declaration both are same things
+
+// function division(v1, v2) {
+//   console.log(v1 / v2);
+//   return undefined;
+// }
+
+// console.log(division(2, 1)); // 2
+// division(100, 10); //10
+
+// function mujheValueDo() {
+//   console.log('Jeet');
+//   return 21211212;
+// }
+
+// var value = mujheValueDo();
+// console.log(value);
+
+// if(true){
+
+// }
+
+// function addition(v1, v2) {
+//   return v1 + v2;
+
+// }
+
+// var result1=addition(3,4)
+
+// var result = addition(2, 2);
+// console.log(result,result1);
+// var arr = []
+// console.log('sasa'.toUpperCase());
+
+// function pataNahi() {
+//   console.log('Hello BHAI');
+
+//   console.log('Hello BHAI2');
+//   console.log('Hello BHAI3');
+//   // return 'Jeet';
+//   console.log('Hello BHAI4');
+//   // return undefined; ye implicitly hota hai jab aap return specify nai karte
+// }
+
+// var value = pataNahi();
+// console.log(value);
+// console.log('I Am Out of a function');
+
+// function validateUserIsNotJeet(userName) {
+//   if (userName === 'Jeet') {
+//     console.log('You Are not allowed');
+//     return;
+//   }
+
+//   console.log('Welcome ' + userName + ' To Saad Account');
+// }
+
+// validateUserIsNotJeet('Jeet');
+
+// function addition(){
+//   return 2+2;
+// }
+
+// var addition = function(){
+//   return 2+2
+// }
+
+// addition();
+
+// console.log('Hello World');
+
+// alert('hellow rold');
+// alert('hellow rold');
+
+// alert('hellow rold');
+
+// alert('hellow rold');
+// Assignment 1 Solution
+// function add(a, b) {
+//   var value1 = a;
+//   var value2 = b;
+//   var result = value1 + value2;
+//   console.log(result);
+// }
+
+// add(2, 2);
+// add(4, 23);
