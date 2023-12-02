48f4a8857f5224975c71ae84d037247333b46f6c
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-12-02 19:28:36 +0500

    class 35 code and assignments uploaded

---
+++ 35-Class 35 - Javascript Basics 11/Assignment.md
@@ -0,0 +1,23 @@
+### Assignment 1: Simple Sum
+
+Create a function named `add` that takes two parameters, `a` and `b`, and logs their sum to the console. Call the function with different numbers to ensure it works.
+
+### Assignment 2: Greetings
+
+Write a function called `greet` that takes a `name` as a parameter and logs a greeting message to the console. Call the function with different names.
+
+### Assignment 3: Double It
+
+Define a function named `double` that takes a number as a parameter and logs its double to the console. Test the function with various numbers.
+
+### Assignment 4: Even or Odd Checker
+
+Create a function named `isEven` that takes a number as a parameter and logs "Even" to the console if the number is even and "Odd" if it's odd. Test the function with different numbers.
+
+### Assignment 5: Max of Two Numbers
+
+Define a function named `findMax` that takes two numbers as parameters and logs the larger number to the console. Test the function with different pairs of numbers.
+
+### Assignment No 6: JavaScript Times Table Printer
+
+Create a simple JavaScript function called `printTimesTable` that takes a number as input. This function should display the multiplication table for that number in a user-friendly way on the console. Make sure the output is easy to understand for everyone.
---
+++ 35-Class 35 - Javascript Basics 11/script.js
@@ -0,0 +1,116 @@
+// DRY - Donot Repeat Yourself
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+// {
+//   console.log(1);
+//   console.log(2);
+//   console.log(3);
+// }
+// //Block of code
+// {
+//   console.log(1);
+//   console.log(2);
+//   console.log(3);
+// }
+// {} // this is a block or iske andar jo code aayega is poore ko ham kahenge block of code
+// {
+//   console.log(1);
+//   console.log(2);
+//   console.log(3);
+// }
+
+// var myNum = 100;
+
+// console.log(myNum);
+// console.log(myNum);
+// console.log(myNum);
+// console.log(myNum);
+
+// function myFunction(){
+//   console.log(1);
+//   console.log(2);
+//   console.log(3);
+// }
+
+// function myFunction() {
+//   //Statement 1
+//   //Statement 2
+// }
+// for functions declaration and defination is same thing
+
+// var myvar; //declaration
+// myvar = 12;// defination
+
+// var myvar2=12 //declaration and defination both working
+// console.log(1);
+// console.log(2);
+// console.log(3)
+
+// console.log(1);
+// console.log(2);
+// console.log(3)
+
+// console.log(1);
+// console.log(2);
+// console.log(3)
+// var myVar2;
+// myVar2="Saad"
+// console.log(myVar2)
+// function myPrintFunction(myVar) {
+//   console.log(myVar);
+// }
+
+// myPrintFunction('Jeet Kumar');
+// myPrintFunction('Mohsin');
+// myPrintFunction('Ajmal');
+// myPrintFunction('Hanif');
+
+// function addition(num1,num2) {
+//   console.log(num1 + num2);
+// }
+
+// addition(1, 2);
+// addition(11, 2);
+// addition(3, 10);
+
+// function multiplyMyValues(parameter1, parameter2) {
+//   var result = parameter1 * parameter2;
+//   console.log(result);
+// }
+
+// multiplyMyValues(1, 10); //arguments
+// multiplyMyValues(2, 20);
+
+// function multiplication(parameter1, parameter2) {
+//   var result = parameter1 * parameter2;
+//   console.log(result);
+// }
+
+// multiplication(20, 40);
+// multiplication(50, 100);
+// multiplication(500, 100);
+// var parameter1 = 20;
+// var parameter2 = 40;
+// var result = parameter1 * parameter2;
+// console.log(result);
+
+// var parameter1 = 50;
+// var parameter2 = 100;
+// var result = parameter1 * parameter2;
+// console.log(result);
+
+// var parameter1 = 500;
+// var parameter2 = 100;
+// var result = parameter1 * parameter2;
+// console.log(result);
