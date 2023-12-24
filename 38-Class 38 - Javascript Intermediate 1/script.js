ce227dfd5475dc4ed86a391ec793b540711473f6
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-12-24 17:39:32 +0500

    class 38 code and content uploaded

---
+++ 38-Class 38 - Javascript Intermediate 1/script.js
@@ -0,0 +1,106 @@
+// console.log('INTERMEDIATE JAVASCRIPT PART 1');
+// console.log('OBJECTS IN JAVASCRIPT');
+// var hanifObject = {
+//   var name = 'Hanif';
+//   var age = 26;
+//   var cityName = 'Hyderabad';
+
+//   var eatingFood = function () {
+//     console.log('I AM Eating Food');
+//   };
+// }
+
+// //This IS cALLED Regular Function
+// function getEducation1() {
+//   console.log('Geting Education From University 1');
+// }
+
+// //This IS cALLED Function Expression
+// var getEducation2 = function () {
+//   console.log('Geting Education From University 2');
+// };
+
+// getEducation1();
+// getEducation2();
+// var age = 28 this is normal variable
+// var hanifObject = {
+//   name: 'Hanif', this is object variable and it is called property
+//   age: 26,
+//   cityName: 'Hyderabad',
+
+//   eatingFood: function () {
+//     console.log('I AM Eating Food');
+//   },
+// };
+
+// var emptyArr = [1, 'Jeet Kumar'];
+
+// console.log(emptyArr[1]);
+
+// var emptyObj = { one: 1, name: 'Jeet Kumar', age: 28 };
+
+// console.log(emptyObj['age']);
+
+// var arr = [1, 2, 'jeet'];
+// console.log(arr[0]);
+
+// var obj = {
+//   one: 1,
+//   two: 2,
+//   name: 'jeet',
+// };
+
+// console.log(obj.one);
+// var arr = [1, 2, 3];
+
+// console.log(arr);
+// var obj = {
+//   key: 'value',
+//   key2: 'value2',
+//   key3: 'value3',
+// };
+
+// console.log(obj);
+
+// console.log(console);
+
+var age = 28;
+
+function drivingCar1() {
+  console.log('he is driving a car');
+}
+
+var drivingCar2 = function () {
+  console.log('he is driving a car');
+};
+
+// var saadObj = {
+//   age: 28,
+
+//   drivingCar2: function () {
+//     console.log('he is driving a car from a object');
+//   },
+// };
+
+// drivingCar2();
+// saadObj.drivingCar2();
+// console.log(age);
+// console.log(saadObj.age);
+
+// var obj = {
+//   name: 'jeet kumar',
+
+//   printName: function () {
+//     console.log('hey this is jeet kumar');
+//   },
+// };
+
+// obj.printName();
+
+function name1(parameter1, parametere2) {
+  console.log(parameter1);
+}
+
+name1(2121);
+
+console.log(name1.length);
