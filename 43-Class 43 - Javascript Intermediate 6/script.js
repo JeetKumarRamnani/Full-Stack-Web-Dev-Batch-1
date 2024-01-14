f37895c6b93a260e9d60c3788e24d7dcfb647a88
Jeet Kumar Ramnani <110333842+JeetKumarRamnani@users.noreply.github.com>
2024-01-14 11:25:15 +0000

    class 43 code and content uploaded

--- 43-Class 43 - Javascript Intermediate 6/script.js
+++ 43-Class 43 - Javascript Intermediate 6/script.js
@@ -1,112 +1,112 @@
-// console.log('Javascript Intermediate 5');
-// console.log(" my name is \t jeet ")
+// console.log('Javascript Intermediate 6');
 
-// var userDefinedObject = {
-//   name: 'jeet',
-//   add(a, b) {
-//     return a + b;
-//   },
-// };
+// Chaining Of Methods
+// obj.m1().m2().m3().m4()
 
-// var result = userDefinedObject.add(4, 4);
+// var str = 'Hello-World-Jeet-Kumar';
+// console.log('1 2'.charAt(0));
 
-// console.log(result);
+// var arr = ['1', '2', '3'];
+// console.log(arr);
+// var str = arr.join(' ');
+// console.log(str);
+// var convertToArray = str.split(' ');
+// console.log(convertToArray);
 
-//Predefined Objects
+// console.log(arr.join(" ").split(" "))
 
-//String Object
-//Array Object
-//Date Object
-//Math Object
-//Document Object
-//Window
+// var arr = [1, 2, 3, 4, 5];
+// console.log(arr);
 
-// var str = 'HElLo My Name My Is Jeet';
-// var str2 = 'i live in hyderabad';
+// arr.pop();
+// console.log(arr);
+// arr.push(8);
+// console.log(arr);
 
-// console.log(str.length);
-// console.log(str.toUpperCase());
-// console.log(str.toLowerCase());
-// console.log(str.indexOf('My'));
-// console.log(str.lastIndexOf('My'));
-// console.log(str + ' ' + str2 + ' ' + 'which is in pakistan');
-// console.log(str.concat(' ', str2, ' ', 'which is in pakistan'));
+// var str = 'jeet kumar';
 
-// var str = 'hello-i-am-jeet-kumar';
-// console.log(str.slice(-6, -1));
-// console.log(str.substring(0, 5));
-// console.log(str.substr(0,5))
-// console.log(str.trim());
-// console.log(str.trim());
-// console.log(str.trim());
+// console.log(str.slice(0, 4));
 // console.log(str);
 
-// console.log(str.split("-"));
-
-//Array And Its Methods
-
-// var arr = [1, 2, 3];
-// var arr2 = new Array(1, 2, 3);
-// console.log(arr);
-// Array Methods
-//push method
-//pop method
-//shift
-//unshift
-//slice
-//concat
-//join
-//splice
-
-// var arr = [1, 2, 3, 4, 5];
-
-// arr.push(6);
-// arr.push(7);
-// arr.push(8);
-// arr.push(9);
-
-// arr.pop();
-// arr.pop();
-// arr.pop();
-// arr.pop();
-// arr.pop();
-// arr.pop();
-// arr.pop();
-// arr.pop();
-// arr.pop();
-
-// var arr = [1, 2, 3, 4, 5];
-// arr.unshift(0);
-// arr.shift();
-// arr.shift();
-
-// console.log(arr);
-
-// var str = 'hello g'
-// console.log(str.toUpperCase());
-// console.log(str)
-
-// let arr = [1, 2, 3, 4, 5];
-// let slicedArr = arr.slice(1, 4);
-// console.log(slicedArr); // [2, 3, 4]
-// console.log(arr);
-
 // var arr = [1, 2, 3, 4, 5, 6];
-// // console.log(arr.splice(2, 4, 'jeet'));
-// // console.log(arr)
-// console.log(arr.pop());
+// console.log(arr);
+// var slicedArr = arr.slice(0);
+// console.log(slicedArr);
 // console.log(arr);
 
-//Count Characters And Words In A String
+// var arr = [1, 2, 3, 4];
+// console.log(arr);
+// var arr2 = arr;
 
-function countCharAndWords() {
-    var userString = 'Hello I AM JEET KUMAR';
-    var charCount = userString.length;
-    var wordCount = userString.split(' ').length;
-    console.log(userString);
-    console.log('Characters Are ' + charCount);
-    console.log('Words Are ' + wordCount);
-  }
-  
-  countCharAndWords();
-  
+// arr2[2] = ' jeet';
+
+// console.log(arr, arr2);
+// var copiedArr = arr.slice();
+// console.log(copiedArr);
+// copiedArr[8] = 'kapil';
+// console.log(arr, arr2, copiedArr);
+
+// var value = 20;
+// var value2 = value;
+// value2 = 30;
+// console.log(value);
+// var arr = [1, 2];
+// var arr2 = arr.slice();
+// arr2[2] = 3;
+
+// console.log(arr);
+// console.log(arr2);
+
+// var arr = [1, 2,3,4,5,6];
+// arr.splice(3, 2, 7);
+// console.log(arr);
+
+// var date = "27-May-2023"
+
+// 1995/23/8
+
+// var date = new Date();
+
+// console.log(date.toDateString());
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleDateString());
+// console.log(new Date())
+
+// var date = new Date('24 May 2023');
+// var date = new Date(2012,11,23);
+
+// 1 second = 1000millisecond
+
+// console.log(date);
+
+// var date = new Date(2018, 11, 30);
+
+// var arr  = ["January","fEB","March","April","mAY","jUNE","July","August","Sept","Oct","November","December"]
+// console.log(arr[11])
+
+// var arr = [
+//   'Sunday',
+//   'Monday',
+//   'Tuesday',
+//   'Wednesday',
+//   'Thursday',
+//   'Friday',
+//   'Saturday',
+// ];
+// console.log(arr[4]);
+
+// // Month Ko 0 Se Count Karta Hai
+// var date = new Date();
+// console.log(date.getDay());
+
+var date = new Date();
+// date.setDate(1);
+// date.setMonth(7);
+console.log(date.getDate());
+console.log(date.getMonth());
+console.log(date.getFullYear());
+console.log(date.getSeconds());
+console.log(date.getMilliseconds());
+console.log(date.getTime());
+
+console.log(date);
