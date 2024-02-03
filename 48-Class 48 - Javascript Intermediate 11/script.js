1320ade40825c6ebf698acb61d1409110230cb65
Jeet Kumar Ramnani <110333842+JeetKumarRamnani@users.noreply.github.com>
2024-02-03 17:56:22 +0000

    code uploaded of class 48

---
+++ 48-Class 48 - Javascript Intermediate 11/script.js
@@ -0,0 +1,72 @@
+// var body = document.body
+// var heading= body.childNodes[1]
+// console.log(heading)
+// body.removeChild(heading)
+// var body = document.body;
+// var heading = document.createElement("h1");
+// // var content = document.createTextNode("Heading 1")
+// // heading.appendChild(content)
+// body.appendChild(heading)
+
+// console.log(heading)
+
+// var body = document.body;
+// var heading = document.createElement("h1");
+// // heading.innerHTML = '<a href="#">Heading </a>'
+// // heading.innerText = '<a href="#">Heading </a>'
+// heading.textContent = '<a href="#">Heading </a>'
+// // heading.textContent= "Heading 1"
+
+// body.appendChild(heading)
+
+// console.dir(heading)
+
+// var body = document.body;
+// var heading = document.createElement("h1");
+
+// // heading.innerText="       Hello        "
+// heading.textContent="     Hello 2       "
+
+// body.appendChild(heading)
+
+// console.dir(heading)
+
+
+
+
+
+var arr = [1,2,3,4,5]
+
+// for(var i=0; i<arr.length; i++){
+//     console.log(arr[i])
+// }
+var result=0;
+for(var arrElement of arr){
+   result+=arrElement;
+
+}
+
+console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
