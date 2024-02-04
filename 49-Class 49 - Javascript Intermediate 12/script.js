78e043f6a7ce1e1deee2cbfb2bd301bb63d57c76
Jeet Kumar Ramnani <110333842+JeetKumarRamnani@users.noreply.github.com>
2024-02-04 21:43:25 +0000

    class 49 code uploaded

---
+++ 49-Class 49 - Javascript Intermediate 12/script.js
@@ -0,0 +1,20 @@
+// var arr = [1,2,3,4,5];
+// // for(var val of arr){
+// //     console.log(val)
+// // }
+
+// var obj= {
+
+//     firstName:"jeet",
+//     lastName:"Kumar"
+
+// }
+
+// for(var key in obj){
+//     console.log(obj[key])
+// }
+
+
+// for(var index in arr){
+//     console.log(index)
+// }
