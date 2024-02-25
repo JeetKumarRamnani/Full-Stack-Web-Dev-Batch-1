34c50fdf58c576ad2852f07024d21490e394b915
Jeet Kumar Ramnani <110333842+JeetKumarRamnani@users.noreply.github.com>
2024-02-25 10:25:22 +0000

    class 52 code added

---
+++ 52-Class 52 - Javascript Intermediate 15/script.js
@@ -0,0 +1,40 @@
+// var mth = Math;
+
+// console.log(Math)
+
+// math.ceil,math.floor and most important math.random
+
+
+var num = 12.99;
+
+
+// console.log(Math.round(num))
+// console.log(Math.ceil(num))
+// console.log(Math.floor(num))
+
+//Math Random Method
+
+// greter then 0 value dega and less then 1 value dega
+
+// 0 and 1 are excluded only gives inbetween 0 and 1 values
+
+// 0.1,0.2,0.05,0.06 0.99 
+// console.log(Math.floor(Math.random() * 11) )
+
+// console.log(Math.floor(Math.random() * 256))
+
+// document.querySelector("#myDiv").style.backgroundColor="rgb(255,100,244)"
+
+
+//Mouse Events
+
+
+
+
+
+
+
+
+
+
+
