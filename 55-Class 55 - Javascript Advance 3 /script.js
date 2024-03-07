8f0d4f7f29768de15550e958e0010debbd246b5f
Jeet Kumar Ramnani <110333842+JeetKumarRamnani@users.noreply.github.com>
2024-03-07 17:42:53 +0000

    class 55 code added

---
+++ 55-Class 55 - Javascript Advance 3 /script.js
@@ -0,0 +1,102 @@
+//Hoisting
+// var jeet; 
+
+// console.log(jeet)
+
+// var jeet = "jeet"
+// let jeet;
+
+
+// console.log(jeet)
+// let jeet = "jeet"
+
+
+// jeet();
+// //Function Declaration Method
+// function jeet(){
+//     console.log("jeet")
+// }
+
+
+
+//Function Expression
+// var jeet;
+// jeet= undefined;
+
+// jeet();
+
+// var jeet = function(){
+//     console.log("jeet")
+// }
+
+
+// var harmeet = "khamneet"
+
+// function jeet(){
+//     var harmeet;
+//     console.log(harmeet)
+//     var harmeet = "harmeet"
+// }
+
+
+// jeet();
+
+
+
+
+
+// var jeet = "hello 'my' \"name\" is jeet"
+// var jeet2= "jeet"
+// var kumar = 'hello'+ jeet2+ 'mr kumar'
+
+// console.log(jeet+kumar)
+
+
+
+
+
+
+// var jeet = `hello my name "is"  'jeet kumar'`
+// console.log(jeet)
+
+
+// var str = "heelo my name is \n jeet kumar"
+// var str2= `heelo my name is 
+//                      jeet kumar`
+
+// console.log(str2)
+
+// let name = "Jeet Kumar";
+// let msg = "Hello Mr " + name;
+
+// console.log(msg)
+
+// String Interpolation
+
+// let name = "Jeet Kumar"
+// let hotelName = "Sarena"
+// let msg = `Hello Mr ${name} to Hotel ${false ? "hi" : "bye"} `
+
+// console.log(msg)
+
+
+
+//Default Parameter
+
+// function jeet(para1=0,para2=0){
+//     console.log(`Para1 Value: ${para1}`)
+//     console.log(`Para2 Value: ${para2}`)
+
+//     console.log(`Para1+Para2: ${para1+para2}`)
+// }
+
+// jeet(10,20);
+
+
+
+
+
+
+
+
+
