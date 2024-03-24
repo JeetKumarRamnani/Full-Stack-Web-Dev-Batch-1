f4b24cd0cf0c21905ee8faa879d2fa7461e641a3
Jeet Kumar Ramnani <110333842+JeetKumarRamnani@users.noreply.github.com>
2024-03-24 18:17:44 +0000

    class 60 code uploaded

---
+++ 60-Class 60 - Javascript Advance 8/script.js
@@ -0,0 +1,43 @@
+// let obj={
+//     "name":"jeet kumar",
+//     age:28,
+
+//     jeet(){
+
+//     },
+// }
+
+
+// let jsonObject = {
+//     "name":"jeet kumar",
+//     "age":28
+// }
+
+
+
+// JSON PROVIDES 2 METHODS TO WORK WITH JSON 
+
+// JSON.parse() json string ko object me convert karta hai
+// JSON.stringify() kisi bhi js object ko json me convert karta h
+
+
+// const jsonString = '{"name": "John", "age": 30}';
+// const jsonObject = JSON.parse(jsonString);
+
+// console.log(jsonObject.name)
+
+// console.log(jsonObject.name); // Output: John
+
+const personObject = { name: 'Alice', age: 25 };
+const jsonStringified = JSON.stringify(personObject);
+
+console.log(jsonStringified); // Output: {"name":"Alice","age":25}
+
+
+
+
+
+
+
+
+
