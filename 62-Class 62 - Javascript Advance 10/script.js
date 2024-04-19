16a9e831a2bde69938e3cf6b23601e1f2ec73bdb
Jeet Kumar Ramnani <110333842+JeetKumarRamnani@users.noreply.github.com>
2024-04-19 20:18:04 +0000

    class 62 code uploaded

---
+++ 62-Class 62 - Javascript Advance 10/script.js
@@ -0,0 +1,60 @@
+// document.title= "Class No 62"
+
+// let namee = document.querySelector("#name");
+// let para=  document.querySelector("#para")
+
+// // namee.addEventListener("change",function(){
+// //     console.log("change wala event chal gaya")
+// // })
+
+// namee.addEventListener("change",function(){
+//     console.log("input wala event chal gaya")
+//     para.innerText = namee.value
+// })
+
+
+// document.querySelector("button").addEventListener("click",function(){
+//     location.href="https://google.com"
+    
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let obj= {
+//     name:"jeet",
+//     age:28,
+//     location:"pakistan"
+// }
+
+//  let stringedData = JSON.stringify(obj)
+
+
+
+// localStorage.setItem("myData",stringedData)
+
+// let convertStringedToObject = JSON.parse(stringedData)
+// console.log(typeof convertStringedToObject)
+
+
+
+
+
+
+
+// console.log("hey")
+
